1a6103b741c134fdc57e2501c90a2f79e9682625
srusso <srusso@lpnws4225.in2p3.fr>
2020-10-07 12:13:12 +0200

    same as the previus commit but with the correct version number in BRS

--- firmware/targets/GREB_v2/Version.vhd
+++ firmware/targets/GREB_v2/Version.vhd
@@ -25,7 +25,7 @@ package Version is
 
   constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"21382008"; -- MAKE_VERSION
 
-constant BUILD_STAMP_C : string := "GREB_v2: Vivado v2015.3 (x86_64) Built Tue Oct  6 16:52:15 CEST 2020 by srusso";
+constant BUILD_STAMP_C : string := "GREB_v2: Vivado v2015.3 (x86_64) Built Wed Oct  7 10:22:39 CEST 2020 by srusso";
 
 end Version;
 
--- firmware/targets/GREB_v2_2_seq/Version.vhd
+++ firmware/targets/GREB_v2_2_seq/Version.vhd
@@ -25,7 +25,7 @@ package Version is
 
   constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"21382108"; -- MAKE_VERSION
 
-constant BUILD_STAMP_C : string := "GREB_v2_2_seq: Vivado v2015.3 (x86_64) Built Tue Oct  6 11:36:36 CEST 2020 by srusso";
+constant BUILD_STAMP_C : string := "GREB_v2_2_seq: Vivado v2015.3 (x86_64) Built Wed Oct  7 11:59:54 CEST 2020 by srusso";
 
 end Version;
 
